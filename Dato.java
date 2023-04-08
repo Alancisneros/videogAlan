16039c171eec5fc22b9956040dff5843db23ce64
Alancisneros <68255778+Alancisneros@users.noreply.github.com>
2023-04-07 23:27:42 -0600

    Add files via upload

---
+++ Dato.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+/**
+ *
+ * @author alancisneros
+ */
+public class Dato implements Serializable{
+    int punta;
+    Ficha f;
+    ArrayList jugadores;
+    
+ public Dato(Ficha f,int punta,ArrayList jugadores ){
+ this.punta=punta;this.f=f;this.jugadores=jugadores;
+ }   
+}
